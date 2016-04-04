02b0de359f3653ec1cedcd38a4a56a6905d872e5
sgholve <sandeep.gholve@profitbricks.com>
2016-04-04 13:43:16 +0200

    Fibonacci tail recursive function.

--- src/func_prog/chapter2/MyModule.scala
+++ src/func_prog/chapter2/MyModule.scala
@@ -15,6 +15,21 @@ object MyModule {
     go(num, 1)
   }
 
+  /**
+    * Calculating nth Fibonacci number.
+    * @param num position of nth Number in the Fibonacci Series.
+    * @return nth Number in the Fibonacci Series.
+    */
+  def fibonacci(num: Int): Int = {
+    def go(count: Int, first: Int, second: Int): Int = {
+      if (count == 0) first
+      else go(count-1, second, first+second)
+    }
+
+    if (num == 0) 0
+    else go(num-1, 0, 1)
+  }
+
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
---
+++ src/func_prog/chapter2/Test.scala
@@ -0,0 +1,13 @@
+package func_prog.chapter2
+
+object Test {
+  def main(args: Array[String]) {
+    val a = -42
+    println(MyModule.abs(a))
+    println("Hello Scala")
+    println(MyModule.fibonacci(8))
+    val num = 5
+    val msg = "Factorial of %d is %d"
+    msg.format(num, MyModule.factorial(num))
+  }
+}
