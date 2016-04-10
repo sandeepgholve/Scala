3ac0cfd3d678d96cc163e241e5770876cf22f638
Sandeep Gholve <sandeep.gholve@profitbricks.com>
2016-04-10 15:04:55 +0200

    Factorial

--- src/func_prog/chapter2/MyModule.scala
+++ src/func_prog/chapter2/MyModule.scala
@@ -28,14 +28,34 @@ object MyModule {
 
     go(num, 0, 1)
   }
+  
+  def fib(n: Int): Int = {
+    def go(first: Int, second: Int, count: Int): Int = {
+      if (count <= 0) first
+      else go(second, first + second, count-1)
+    }
+    go(0, 1, n-1)
+  }
 
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
   }
 
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d"
+    msg.format(n, factorial(n))
+  }
+  
+  private def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
   def main(args: Array[String]): Unit = {
-    println(formatAbs(-42))
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 5, factorial))
+    println(formatResult("nth Fibonacci number ", 5, fib))
   }
 
 }
